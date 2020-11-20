774e0f164de0b6d28dd6e3502c09d31f82f5bfbb
lyellread <lyellread@gmail.com>
2020-11-19 20:52:56 -0800

    Complete DA5

--- data-analysis/data-analysis-5/DA5_t_procedures.R
+++ data-analysis/data-analysis-5/DA5_t_procedures.R
@@ -3,8 +3,22 @@
 # Read in the microbeersW19.csv dataset
 microbeers = read.csv(file.choose(), header = TRUE)
 
+hist(microbeers$abv, 
+     main = "ABV Histogram of Microbrews in the USA", 
+     xlab = "ABV Percentage", 
+     ylab = "Frequency",
+     col = "dodgerblue")
+
 # gives variable names.
 names(microbeers) # gives variable names. 
+
+summary(microbeers$abv)
+
+# Calculate the Sample Standard Deviation sd()
+sd(microbeers$abv)
+
+t.test(microbeers$abv, mu=5,alternative = "two.sided" )
+
 
 # Make an appropriate visual display for abv. 
 # Recall hist() or boxplot()
